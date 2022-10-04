cf7ccaf301a5af1a965cfd80639f5e247e699d9e
Yeela Lifshitz <yeelalifshitz@MacBook-Pro.local>
2022-10-04 13:17:17 +0300

    logs only test

--- src/app/core/services/artworks.service.ts
+++ src/app/core/services/artworks.service.ts
@@ -16,6 +16,7 @@ export class ArtworksService {
   ) { }
 
   getArtworksByDepartment() {
+    console.log('test');
     return this.http.get(environment.baseUrl + "search?departmentId=11&q=sunflowers").pipe(
       mergeMap(res =>
         from(res.objectIDs).pipe(
