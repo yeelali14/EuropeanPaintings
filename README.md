7a8df62449ea1efbed8f1311622ff3e2e05701ee
Yeela Lifshitz <yeelalifshitz@MacBook-Pro.local>
2023-05-07 14:54:53 +0300

    many changes

--- README.md
+++ README.md
@@ -32,3 +32,38 @@ To get more help on the Angular CLI use `ng help` or go check out the [Angular C
 Lady gaga the queen
 team flare :)
 dasdasdasdasdasd
+
+# EuropeanPaintings
+
+![image](https://user-images.githubusercontent.com/52451294/131232589-06f209d5-ce33-48ff-aa3a-6e74fa0de637.png)
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.2.3.
+
+## Development server
+
+1) Install Angular CLI: `npm install -g @angular/cli`
+2) Run `npm install`
+3) Run `ng serve` for a dev server. 
+4) Navigate to `http://localhost:4200/`. The app will automatically reload.
+
+##Elad's history:
+I was born in 1990
+kjhgdkjhsdgfjk
+
+## Further help
+
+To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+
+#PR
+To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+
+#PRTo get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+
+#PRTo get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+
+#PRTo get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+
+#PR
+Lady gaga the queen
+team flare :)
+dasdasdasdasdasd
--- src/app/core/services/artworks.service.ts
+++ src/app/core/services/artworks.service.ts
@@ -71,3 +71,175 @@ function waitForImageToLoad(imageElement) {
   })
 }
 
+
+
+
+getArtworksByDepartment() {
+  return this.http.get(environment.baseUrl + "search?departmentId=11&q=sunflowers").pipe(
+    mergeMap(res =>
+      from(res.objectIDs).pipe(
+        mergeMap(objectID => this.http.get(environment.baseUrl + "objects/" + objectID).pipe(
+          map(artwork => {
+            this.getImageColour(artwork);
+            return this.artworks;
+          })
+        ))
+      )
+    )
+  )
+}
+
+
+getImageColour(artwork: any) {
+  let rgb = [];
+  let palette = [];
+  let paletteColours = [];
+  const tempArtworks = new Array<Artwork>();
+  const colorThief = new ColorThief();
+  const img = new Image();
+
+  img.addEventListener('load', () => {
+
+  })
+
+  let imageURL = artwork.primaryImageSmall;
+  let googleProxyURL = 'https://images1-focus-opensocial.googleusercontent.com/gadgets/proxy?container=focus&refresh=2592000&url=';
+  img.crossOrigin = 'Anonymous';
+  img.src = googleProxyURL + encodeURIComponent(imageURL);
+  waitForImageToLoad(img).then(() => {
+    rgb = colorThief.getColor(img);
+    palette = colorThief.getPalette(img, 5);
+    if(rgb && rgb.length > 0 && palette && palette.length > 0) {
+      let color = "#" + ((1 << 24) + (rgb[0] << 16) + (rgb[1] << 8) + rgb[2]).toString(16).slice(1);
+      palette.forEach(x => {
+        let pColour = "#" + ((1 << 24) + (x[0] << 16) + (x[1] << 8) + x[2]).toString(16).slice(1);
+        paletteColours.push(pColour);
+      });
+      let artworkObject = new Artwork(artwork.objectID, artwork.primaryImageSmall, color, paletteColours);
+      this.artworks.push(artworkObject);
+    }
+  });
+}
+
+}
+
+
+function waitForImageToLoad(imageElement) {
+return new Promise(resolve => {
+  imageElement.onload = resolve;
+})
+}
+
+
+getArtworksByDepartment() {
+  return this.http.get(environment.baseUrl + "search?departmentId=11&q=sunflowers").pipe(
+    mergeMap(res =>
+      from(res.objectIDs).pipe(
+        mergeMap(objectID => this.http.get(environment.baseUrl + "objects/" + objectID).pipe(
+          map(artwork => {
+            this.getImageColour(artwork);
+            return this.artworks;
+          })
+        ))
+      )
+    )
+  )
+}
+
+
+getImageColour(artwork: any) {
+  let rgb = [];
+  let palette = [];
+  let paletteColours = [];
+  const tempArtworks = new Array<Artwork>();
+  const colorThief = new ColorThief();
+  const img = new Image();
+
+  img.addEventListener('load', () => {
+
+  })
+
+  let imageURL = artwork.primaryImageSmall;
+  let googleProxyURL = 'https://images1-focus-opensocial.googleusercontent.com/gadgets/proxy?container=focus&refresh=2592000&url=';
+  img.crossOrigin = 'Anonymous';
+  img.src = googleProxyURL + encodeURIComponent(imageURL);
+  waitForImageToLoad(img).then(() => {
+    rgb = colorThief.getColor(img);
+    palette = colorThief.getPalette(img, 5);
+    if(rgb && rgb.length > 0 && palette && palette.length > 0) {
+      let color = "#" + ((1 << 24) + (rgb[0] << 16) + (rgb[1] << 8) + rgb[2]).toString(16).slice(1);
+      palette.forEach(x => {
+        let pColour = "#" + ((1 << 24) + (x[0] << 16) + (x[1] << 8) + x[2]).toString(16).slice(1);
+        paletteColours.push(pColour);
+      });
+      let artworkObject = new Artwork(artwork.objectID, artwork.primaryImageSmall, color, paletteColours);
+      this.artworks.push(artworkObject);
+    }
+  });
+}
+
+}
+
+
+function waitForImageToLoad(imageElement) {
+return new Promise(resolve => {
+  imageElement.onload = resolve;
+})
+}
+
+
+getArtworksByDepartment() {
+  return this.http.get(environment.baseUrl + "search?departmentId=11&q=sunflowers").pipe(
+    mergeMap(res =>
+      from(res.objectIDs).pipe(
+        mergeMap(objectID => this.http.get(environment.baseUrl + "objects/" + objectID).pipe(
+          map(artwork => {
+            this.getImageColour(artwork);
+            return this.artworks;
+          })
+        ))
+      )
+    )
+  )
+}
+
+
+getImageColour(artwork: any) {
+  let rgb = [];
+  let palette = [];
+  let paletteColours = [];
+  const tempArtworks = new Array<Artwork>();
+  const colorThief = new ColorThief();
+  const img = new Image();
+
+  img.addEventListener('load', () => {
+
+  })
+
+  let imageURL = artwork.primaryImageSmall;
+  let googleProxyURL = 'https://images1-focus-opensocial.googleusercontent.com/gadgets/proxy?container=focus&refresh=2592000&url=';
+  img.crossOrigin = 'Anonymous';
+  img.src = googleProxyURL + encodeURIComponent(imageURL);
+  waitForImageToLoad(img).then(() => {
+    rgb = colorThief.getColor(img);
+    palette = colorThief.getPalette(img, 5);
+    if(rgb && rgb.length > 0 && palette && palette.length > 0) {
+      let color = "#" + ((1 << 24) + (rgb[0] << 16) + (rgb[1] << 8) + rgb[2]).toString(16).slice(1);
+      palette.forEach(x => {
+        let pColour = "#" + ((1 << 24) + (x[0] << 16) + (x[1] << 8) + x[2]).toString(16).slice(1);
+        paletteColours.push(pColour);
+      });
+      let artworkObject = new Artwork(artwork.objectID, artwork.primaryImageSmall, color, paletteColours);
+      this.artworks.push(artworkObject);
+    }
+  });
+}
+
+}
+
+
+function waitForImageToLoad(imageElement) {
+return new Promise(resolve => {
+  imageElement.onload = resolve;
+})
+}
