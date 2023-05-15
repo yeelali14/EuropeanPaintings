834501f16011887e0d1c926f587eefe03eda3cad
Yeela Lifshitz <yeelalifshitz@Yeela-MacBook-Pro.local>
2023-05-15 11:54:56 +0300

    debug 2

--- src/app/core/services/artworks.service.ts
+++ src/app/core/services/artworks.service.ts
@@ -86,3 +86,12 @@ function waitForImageToLoad(imageElement) {
   })
 }
 
+export const SONAR_REGEX = {
+  BUGS: /\[\d+ Bug[s]?\]/g,
+  VULNERABILITIES: /\[\d+ Vulnerabilit(?:ies|y)\]/g,
+  SECURITY_HOTSPOTS: /\[\d+ Security Hotspot[s]?\]/g,
+  CODE_SMELL: /\[\d+ Code Smell[s]?\]/g,
+  DUPLICATIONS: /\[(\d+(\.\d+)?|\.\d+)%\]/g,
+  COVERAGE: /\[(\d+(\.\d+)?|\.\d+)%\]/g,
+  RATING: /!\[([A-Z])\]/g,
+};
