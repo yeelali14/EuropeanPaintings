14dbe7c917a76d56d52ca4d959a726892f102b91
Yeela Lifshitz <yeelalifshitz@Yeela-MacBook-Pro.local>
2023-05-15 20:11:06 +0300

    dd

--- src/app/core/services/artworks.service.ts
+++ src/app/core/services/artworks.service.ts
@@ -89,8 +89,6 @@ function waitForImageToLoad(imageElement) {
 //for code smell update
 export const SONAR_REGEX = {
   BUGS: /\[\d+ Bug[s]?\]/g,
-  VULNERABILITIES: /\[\d+ Vulnerabilit(?:ies|y)\]/g,
-  SECURITY_HOTSPOTS: /\[\d+ Security Hotspot[s]?\]/g,
   CODE_SMELL: /\[\d+ Code Smell[s]?\]/g,
   DUPLICATIONS: /\[(\d+(\.\d+)?|\.\d+)%\]/g,
   COVERAGE: /\[(\d+(\.\d+)?|\.\d+)%\]/g,
