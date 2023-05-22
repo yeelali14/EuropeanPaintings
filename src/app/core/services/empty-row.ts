373d74f66d880a765a3437ac6d0c6fc435f39c04
Yeela Lifshitz <yeelalifshitz@Yeela-MacBook-Pro.local>
2023-05-22 16:06:33 +0300

    no bugs

--- src/app/core/services/empty-row.ts
+++ src/app/core/services/empty-row.ts
@@ -1,3 +1,2 @@
 //dasdasdas
 //dasd
-dsadasda
--- src/app/core/services/emtyFile.ts
+++
@@ -1,7 +0,0 @@
-export const SONAR_REGEX = {
-  VULNERABILITIES: /\[\d+ Vulnerabilit(?:ies|y)\]/g,
-  CODE_SMELL: /\[\d+ Code Smell[s]?\]/g,
-  DUPLICATIONS: /\[(\d+(\.\d+)?|\.\d+)%\]/g,
-  COVERAGE: /\[(\d+(\.\d+)?|\.\d+)%\]/g,
-  RATING: /!\[([A-Z])\]/g,
-};
