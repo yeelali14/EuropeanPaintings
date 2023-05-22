60d2f7011c1f0b18d4a531f5ec47d5a8b841b6e6
Yeela Lifshitz <yeelalifshitz@Yeela-MacBook-Pro.local>
2023-05-22 15:46:15 +0300

    test

--- src/app/core/services/emtyFile.ts
+++ src/app/core/services/emtyFile.ts
@@ -1,6 +1,5 @@
 export const SONAR_REGEX = {
   VULNERABILITIES: /\[\d+ Vulnerabilit(?:ies|y)\]/g,
-  SECURITY_HOTSPOTS: /\[\d+ Security Hotspot[s]?\]/g,
   CODE_SMELL: /\[\d+ Code Smell[s]?\]/g,
   DUPLICATIONS: /\[(\d+(\.\d+)?|\.\d+)%\]/g,
   COVERAGE: /\[(\d+(\.\d+)?|\.\d+)%\]/g,
