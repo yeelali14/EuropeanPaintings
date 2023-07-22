a85df16c47eabe3b17ee23b42b30f24175845ded
Yeela Lifshitz <yeelalifshitz@Yeela-MacBook-Pro.local>
2023-07-22 21:15:09 +0300

    second commit

--- src/app/core/services/empty-row.ts
+++ src/app/core/services/empty-row.ts
@@ -41,4 +41,4 @@ const ttdasd = {
     isFullyInstalled: true,
   },
 };
-//console.log('yeela-debug githubEvent: ', githubEvent);dasdasdasadasdasd
+//console.log('yeela-debug githubEvent: ', githubEvent);dasdasdasadasdasdsaSasaS
