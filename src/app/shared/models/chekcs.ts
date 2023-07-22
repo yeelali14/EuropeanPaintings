522f3458f4c647dfa2d64037e38257f505bc48f4
Yeela Lifshitz <yeelalifshitz@Yeela-MacBook-Pro.local>
2023-07-22 19:34:00 +0300

    second commit

--- src/app/shared/models/chekcs.ts
+++ src/app/shared/models/chekcs.ts
@@ -810,7 +810,7 @@ const workflowRuns = {
   },
 };
 
-//dhaskdjhadasdafdsf
+//dhaskdjhadasdafdsfdada
 const workflowRuns2 = {
   status: 200,
   url: 'https://api.github.com/repos/yeelali14/pasha/actions/workflows/.github%2Fworkflows%2Fexample.yml/runs?branch=yeelali14-patch-12',
