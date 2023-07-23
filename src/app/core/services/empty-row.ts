7e24668a36a3f2461e0c26679657fee4a6dd06f5
Yeela Lifshitz <yeelalifshitz@Yeela-MacBook-Pro.local>
2023-07-23 13:45:44 +0300

    k

--- src/app/core/services/empty-row.ts
+++ src/app/core/services/empty-row.ts
@@ -1,44 +1,46 @@
-const tt2 = {
-  timestamp: '2023-07-22T16:35:47.741Z',
-  level: 'info',
-  message:
-    'set prState: github-yeelali14/EuropeanPaintings/867 with payload: {"prCommands":[],"isFullyInstalled":true,"trackedWorkflows":{"branch":"","wokflow_id":".github/workflows/example.yml","check_name":"blabla"},"org":"github-yeelali14","prPath":"EuropeanPaintings/867"}',
-  ctx: {
-    environment: 'local',
-    app: 'github-actions-consumer',
-    prCommands: [],
-    watchers: { watchPREvents: { labels: true }, watchFilters: {} },
-    prPath: 'EuropeanPaintings/867',
-    trackedWorkflows: {
-      branch: '',
-      wokflow_id: '.github/workflows/example.yml',
-      check_name: 'blabla',
-    },
-    org: 'github-yeelali14',
-    isFullyInstalled: true,
+const workflowResult = {
+  status: 200,
+  url: 'https://api.github.com/repos/yeelali14/pasha/actions/workflows/.github%2Fworkflows%2Fexample.yml',
+  headers: {
+    'access-control-allow-origin': '*',
+    'access-control-expose-headers':
+      'ETag, Link, Location, Retry-After, X-GitHub-OTP, X-RateLimit-Limit, X-RateLimit-Remaining, X-RateLimit-Used, X-RateLimit-Resource, X-RateLimit-Reset, X-OAuth-Scopes, X-Accepted-OAuth-Scopes, X-Poll-Interval, X-GitHub-Media-Type, X-GitHub-SSO, X-GitHub-Request-Id, Deprecation, Sunset',
+    'cache-control': 'private, max-age=60, s-maxage=60',
+    connection: 'close',
+    'content-encoding': 'gzip',
+    'content-security-policy': "default-src 'none'",
+    'content-type': 'application/json; charset=utf-8',
+    date: 'Sun, 23 Jul 2023 10:39:33 GMT',
+    etag: 'W/"68e9069d8805679b95937e786139327b4ff4346f14faace631af8db4a74e5c1e"',
+    'referrer-policy':
+      'origin-when-cross-origin, strict-origin-when-cross-origin',
+    server: 'GitHub.com',
+    'strict-transport-security': 'max-age=31536000; includeSubdomains; preload',
+    'transfer-encoding': 'chunked',
+    vary: 'Accept, Authorization, Cookie, X-GitHub-OTP, Accept-Encoding, Accept, X-Requested-With',
+    'x-content-type-options': 'nosniff',
+    'x-frame-options': 'deny',
+    'x-github-api-version-selected': '2022-11-28',
+    'x-github-media-type': 'github.v3; format=json',
+    'x-github-request-id': 'DD86:EB96:6A615BB:6BA6DC1:64BD0365',
+    'x-ratelimit-limit': '5000',
+    'x-ratelimit-remaining': '4950',
+    'x-ratelimit-reset': '1690111594',
+    'x-ratelimit-resource': 'core',
+    'x-ratelimit-used': '50',
+    'x-xss-protection': '0',
+  },
+  data: {
+    id: 62113785,
+    node_id: 'W_kwDOJOZy_s4Ds8f5',
+    name: 'example',
+    path: '.github/workflows/example.yml',
+    state: 'active',
+    created_at: '2023-07-04T09:13:40.000Z',
+    updated_at: '2023-07-19T08:11:08.000Z',
+    url: 'https://api.github.com/repos/yeelali14/pasha/actions/workflows/62113785',
+    html_url:
+      'https://github.com/yeelali14/pasha/blob/main/.github/workflows/example.yml',
+    badge_url: 'https://github.com/yeelali14/pasha/workflows/example/badge.svg',
   },
 };
-
-const ttdasd = {
-  timestamp: '2023-07-22T16:35:57.330Z',
-  level: 'info',
-  message:
-    'set prState: github-yeelali14/EuropeanPaintings/867 with payload: {"prCommands":[{"state":"UPDATE_LABELS","args":{"labels":["5 min review"]}}],"watchers":{"watchPREvents":{"labels":true},"watchFilters":{}},"isFullyInstalled":true,"trackedWorkflows":{"branch":"","wokflow_id":".github/workflows/example.yml","check_name":"blabla"},"org":"github-yeelali14","prPath":"EuropeanPaintings/867"}',
-  ctx: {
-    environment: 'local',
-    app: 'github-actions-consumer',
-    prCommands: [
-      { state: 'UPDATE_LABELS', args: { labels: ['5 min review'] } },
-    ],
-    watchers: { watchPREvents: { labels: true }, watchFilters: {} },
-    prPath: 'EuropeanPaintings/867',
-    trackedWorkflows: {
-      branch: '',
-      wokflow_id: '.github/workflows/example.yml',
-      check_name: 'blabla',
-    },
-    org: 'github-yeelali14',
-    isFullyInstalled: true,
-  },
-};
-//console.log('yeela-debug githubEvent: ', githubEvent);dasdasdasadasdasdsaSasaS
