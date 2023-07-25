f3b8d4e676e7331c320e84b3851496f655e29848
Yeela Lifshitz <yeelalifshitz@Yeela-MacBook-Pro.local>
2023-07-25 10:23:57 +0300

    commit sha

--- src/app/core/services/empty-row.ts
+++ src/app/core/services/empty-row.ts
@@ -114,3 +114,82 @@ const workflow = {
   workflowId: 62113785,
 };
 //gffg
+const dbRes = {
+  wokflow: '.github/workflows/example.yml',
+  org: 'github-yeelali14',
+  repo: 'EuropeanPaintings',
+  headSha: '31f5115d67a65acc759500411f275aef25f5aa86',
+  prNumber: 869,
+  checkName: 'wow',
+  branch: 'yeelali14-patch-13',
+  workflowId: 62113785,
+};
+
+const inputs1 = {
+  owner: 'github-yeelali14',
+  repo: 'EuropeanPaintings',
+  ref: 'yeelali14-patch-13',
+  headSha: '31f5115d67a65acc759500411f275aef25f5aa86',
+  checkName: 'wow'
+};
+const installationID = 40012869;
+const res = {
+  timestamp: '2023-07-25T07:13:48.057Z',
+  level: 'warn',
+  message:
+    'Failed to fetch check-run wow for github-yeelali14/EuropeanPaintings/yeelali14-patch-13 - 31f5115d67a65acc759500411f275aef25f5aa86, ',
+  ctx: {
+    environment: 'local',
+    app: 'gitstream-events-consumer',
+    name: 'HttpError',
+    status: 404,
+    response: {
+      url: 'https://api.github.com/repos/github-yeelali14/EuropeanPaintings/commits/yeelali14-patch-13/check-runs?check_name=wow&filter=latest',
+      status: 404,
+      headers: {
+        'access-control-allow-origin': '*',
+        'access-control-expose-headers':
+          'ETag, Link, Location, Retry-After, X-GitHub-OTP, X-RateLimit-Limit, X-RateLimit-Remaining, X-RateLimit-Used, X-RateLimit-Resource, X-RateLimit-Reset, X-OAuth-Scopes, X-Accepted-OAuth-Scopes, X-Poll-Interval, X-GitHub-Media-Type, X-GitHub-SSO, X-GitHub-Request-Id, Deprecation, Sunset',
+        connection: 'close',
+        'content-encoding': 'gzip',
+        'content-security-policy': "default-src 'none'",
+        'content-type': 'application/json; charset=utf-8',
+        date: 'Tue, 25 Jul 2023 07:13:48 GMT',
+        'referrer-policy':
+          'origin-when-cross-origin, strict-origin-when-cross-origin',
+        server: 'GitHub.com',
+        'strict-transport-security':
+          'max-age=31536000; includeSubdomains; preload',
+        'transfer-encoding': 'chunked',
+        vary: 'Accept-Encoding, Accept, X-Requested-With',
+        'x-content-type-options': 'nosniff',
+        'x-frame-options': 'deny',
+        'x-github-api-version-selected': '2022-11-28',
+        'x-github-media-type': 'github.v3; format=json',
+        'x-github-request-id': 'EE06:27C4:5546FF:562937:64BF762B',
+        'x-ratelimit-limit': '5000',
+        'x-ratelimit-remaining': '4937',
+        'x-ratelimit-reset': '1690271969',
+        'x-ratelimit-resource': 'core',
+        'x-ratelimit-used': '63',
+        'x-xss-protection': '0',
+      },
+      data: {
+        message: 'Not Found',
+        documentation_url:
+          'https://docs.github.com/rest/checks/runs#list-check-runs-for-a-git-reference',
+      },
+    },
+    request: {
+      method: 'GET',
+      url: 'https://api.github.com/repos/github-yeelali14/EuropeanPaintings/commits/yeelali14-patch-13/check-runs?check_name=wow&filter=latest',
+      headers: {
+        accept: 'application/vnd.github.v3+json',
+        'user-agent':
+          'octokit.js/2.0.7 octokit-core.js/4.2.1 Node.js/16.20.1 (linux; arm64)',
+        authorization: 'tok*****',
+      },
+      request: {},
+    },
+  },
+};
