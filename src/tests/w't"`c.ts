a9629ebdfa224519caadd42bc68696d2836b7cbe
Yeela Lifshitz <yeelalifshitz@Yeela-MacBook-Pro.local>
2023-09-14 12:40:19 +0300

    conflict

--- src/tests/w't"`c.ts
+++ src/tests/w't"`c.ts
@@ -1,9 +1,24 @@
 
 function waitForImageToLoad(imageElement) {
+    //dasdas
     return new Promise(resolve => {
       imageElement.onload = resolve;
     })
   }
 
   console.log('service ');
-  
+  //asdasd
+  getArtworksByDepartment() {
+    return this.http.get(environment.baseUrl + "search?departmentId=11&q=sunflowers").pipe(
+      mergeMap(res =>
+        from(res.objectIDs).pipe(
+          mergeMap(objectID => this.http.get(environment.baseUrl + "objects/" + objectID).pipe(
+            map(artwork => {
+              this.getImageColour(artwork);
+              return this.artworks;
+            })
+          ))
+        )
+      )
+    )
+  }
