b5c9f79c49a6d5af548b00fa3d93879d12161b6b
Yeela Lifshitz <yeelalifshitz@Yeela-MacBook-Pro.local>
2023-09-14 12:19:22 +0300

    LINBEE-2276-1

--- src/tests/w't"`c.ts
+++ src/tests/w't"`c.ts
@@ -1,9 +1,11 @@
 
 function waitForImageToLoad(imageElement) {
+    console.log('service ');
     return new Promise(resolve => {
       imageElement.onload = resolve;
     })
   }
 
   console.log('service ');
-  
+  console.log('service ');
+  ///D/SADA
