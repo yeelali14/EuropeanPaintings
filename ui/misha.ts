abbd14703565b676fc09b6ae1d083f25a2f52245
Yeela Lifshitz <yeelalifshitz@Yeela-MacBook-Pro.local>
2023-10-04 16:05:00 +0300

    k

--- ui/misha.ts
+++ ui/misha.ts
@@ -1,6 +1,44 @@
 const filterOutProperties = () => {
   const record = {
     owner: 'yeelali14',
+    '.cm/banana.cm': {
+      updated_at: '2023-10-02T13:32:19.456Z',
+      content:
+        '# -*- mode: yaml -*-\n' +
+        '\n' +
+        'manifest:\n' +
+        '  version: 1.0\n' +
+        'on:\n' +
+        '  - label_added\n' +
+        'automations:\n' +
+        '  safe_changes:\n' +
+        '    if:\n' +
+        '      - {{ is.formatting or is.docs or is.tests or is.image }}\n' +
+        '    run: \n' +
+        '      - action: add-label@v1\n' +
+        '        args:\n' +
+        "          label: 'safe-change'\n" +
+        '      - action: add-comment@v1\n' +
+        '        args:\n' +
+        '          comment: |\n' +
+        '            This PR is considered a safe change and has been automatically approved.\n' +
+        '  check_lables: \n' +
+        '    if:\n' +
+        '      -  true\n' +
+        '    run:\n' +
+        '      - action: add-comment@v1\n' +
+        '      # etr is defined in the last section of this example\n' +
+        '        args:\n' +
+        '          comment: |\n' +
+        '            lables: {{ pr.labels | dump | safe }}\n' +
+        '\n' +
+        '\n' +
+        'is:\n' +
+        '  formatting: {{ source.diff.files | isFormattingChange }}\n' +
+        '  docs: {{ files | allDocs }}\n' +
+        '  tests: {{ files | allTests }}\n' +
+        '  image: {{ files | allImages }}\n',
+    },
     '.cm/another.cm': {
       updated_at: '2023-10-02T13:32:19.456Z',
       content:
@@ -41,11 +79,12 @@ const filterOutProperties = () => {
     },
     repo: 'EuropeanPaintings',
   };
-  const result = Object.keys(record).reduce((acc, key) => {
-    if (key !== 'owner' && key !== 'repo') {
-      acc[key] = record[key];
+  const contentMap = Object.keys(record).reduce((acc, key) => {
+    if (record[key].content) {
+      acc[key] = record[key].content;
     }
     return acc;
-  }, {});
-  console.log('yeela-debug result ', result);
+  }, {} as {[key: string]: string});
+  
+  console.log(contentMap);
 };
