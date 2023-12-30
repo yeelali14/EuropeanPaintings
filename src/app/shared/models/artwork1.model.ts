3c1e2fefbef4844d39b8075dd2865cb2c2f248b1
Yeela Lifshitz <yeelalifshitz@Yeela-MacBook-Pro.local>
2023-12-30 21:25:27 +0200

    duplication

--- src/app/shared/models/artwork1.model.ts
+++ src/app/shared/models/artwork1.model.ts
@@ -11,3 +11,17 @@ export class Artwork {
         this.DominantPrimaryColour = i_DominantPrimary;
     }
 }
+
+export class Artwork1 {
+    ObjectID: string;
+    ImageURL: string;
+    DominantColour: string;
+    DominantPrimaryColour: Array<string>;
+
+    constructor(i_ObjectID?: string, i_ImageURL?: string, i_DominantColour?: string, i_DominantPrimary?: Array<string>) {
+        this.ObjectID = i_ObjectID;
+        this.ImageURL = i_ImageURL;
+        this.DominantColour = i_DominantColour;
+        this.DominantPrimaryColour = i_DominantPrimary;
+    }
+}
