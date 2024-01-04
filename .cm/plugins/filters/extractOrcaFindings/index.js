9ad09e377c7acaf61e69e892f1887381b06003d5
Yeela Lifshitz <yeelalifshitz@Yeela-MacBook-Pro.local>
2024-01-04 17:52:24 +0200

    plugins error

--- .cm/plugins/filters/extractOrcaFindings/index.js
+++ .cm/plugins/filters/extractOrcaFindings/index.js
@@ -1,43 +1,43 @@
 function getOrcaPropertyRating(lines, lineIdentifierRegex, findingsCellIndex) {
-    const matches = lines.filter(x => x.match(lineIdentifierRegex));
-    const [firstMatch] = matches;
-    const cells = firstMatch.split('|');
-    const [_, high, medium, low, info] = /"High"> ([\d]+).*"Medium"> ([\d]+).*"Low"> ([\d]+).*"Info"> ([\d]+)/
-      .exec(cells[findingsCellIndex])
-      .map(x => parseInt(x));
-    return {high, medium, low, info};
-  }
-  
-  module.exports = (pr) => {
-    let orcaObject = {
-      infrastructure_as_code: { count: null, rating: '' },
-      vulnerabilities: { count: null, rating: '' },
-      secrets: { count: null, rating: '' },
+  const matches = lines.filter(x => x.match(lineIdentifierRegex));
+  const [firstMatch] = matches;
+  const cells = firstMatch.split('|');
+  const [_, high, medium, low, info] = /"High"> ([\d]+).*"Medium"> ([\d]+).*"Low"> ([\d]+).*"Info"> ([\d]+)/
+    .exec(cells[findingsCellIndex])
+    .map(x => parseInt(x));
+  return {high, medium, low, info};
+}
+
+module.exports = (pr) => (pr) {
+  let orcaObject = {
+    infrastructure_as_code: { count: null, rating: '' },
+    vulnerabilities: { count: null, rating: '' },
+    secrets: { count: null, rating: '' },
+  };
+
+  const orcaComment = pr.comments.filter(x => x.commenter.includes('orca-security'));
+
+  if (orcaComment.length) {
+    const orcaCommentArray = orcaComment[0].content.split('\n');
+
+    var rating = getOrcaPropertyRating(orcaCommentArray, /Infrastructure as Code/, 3);
+    orcaObject.infrastructure_as_code = {
+      count: rating.high + rating.medium + rating.low + rating.info,
+      rating,
     };
-  
-    const orcaComment = pr.comments.filter(x => x.commenter.includes('orca-security'));
-  
-    if (orcaComment.length) {
-      const orcaCommentArray = orcaComment[0].content.split('\n');
-  
-      var rating = getOrcaPropertyRating(orcaCommentArray, /Infrastructure as Code/, 3);
-      orcaObject.infrastructure_as_code = {
-        count: rating.high + rating.medium + rating.low + rating.info,
-        rating,
-      };
-  
-      var rating = getOrcaPropertyRating(orcaCommentArray, /Vulnerabilities/, 3);
-      orcaObject.vulnerabilities = {
-        count: rating.high + rating.medium + rating.low + rating.info,
-        rating,
-      };
-  
-      var rating = getOrcaPropertyRating(orcaCommentArray, /Secrets/, 3);
-      orcaObject.secrets = {
-        count: rating.high + rating.medium + rating.low + rating.info,
-        rating,
-      };
-    }
-  
-    return JSON.stringify(orcaObject);
+
+    var rating = getOrcaPropertyRating(orcaCommentArray, /Vulnerabilities/, 3);
+    orcaObject.vulnerabilities = {
+      count: rating.high + rating.medium + rating.low + rating.info,
+      rating,
+    };
+
+    var rating = getOrcaPropertyRating(orcaCommentArray, /Secrets/, 3);
+    orcaObject.secrets = {
+      count: rating.high + rating.medium + rating.low + rating.info,
+      rating,
+    };
   }
+
+  return JSON.stringify(orcaObject);
+}
