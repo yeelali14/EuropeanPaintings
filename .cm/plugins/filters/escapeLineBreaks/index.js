16d867731ae72894d88ff1fe03aecdc8c0ffc341
Yeela Lifshitz <yeelalifshitz@Yeela-MacBook-Pro.local>
2024-01-07 20:44:27 +0200

    fix

--- .cm/plugins/filters/escapeLineBreaks/index.js
+++ .cm/plugins/filters/escapeLineBreaks/index.js
@@ -1,10 +1,16 @@
 module.exports = (desc) => {
   if (desc && desc !== '""' && desc !== "''") {
     console.log("yeela-debug desc: ", desc);
-    const linesTest = desc.split(/\n/); 
-    const lines = desc.split(/\n|\\n/);
-    console.log('yeela-debug split with one backslash', linesTest);
-    console.log('yeela-debug split with two backslash', lines);
+    const linesTest = desc.split(/\n/);
+    const lines = desc.split(/\\n/);
+    console.log("yeela-debug split with one backslash", {
+      linesTest,
+      count: linesTest.length,
+    });
+    console.log("yeela-debug split with two backslash", {
+      lines,
+      count: lines.length,
+    });
     return linesTest;
   }
 
