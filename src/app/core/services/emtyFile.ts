8e1048566d56585b500242b34f2fe2e477dbc9b4
Yeela Lifshitz <yeelalifshitz@Yeela-MacBook-Pro.local>
2024-01-21 18:41:42 +0200

    linbee-5145

---
+++ src/app/core/services/emtyFile.ts
@@ -0,0 +1,10 @@
+const automationResults = {
+    'revertCommands/request_screenshot': [
+      { 'add-label@v1': [Object] },
+      { 'request-changes@v1': [Object] },
+      { 'add-comment@v1': [Object] }
+    ]
+  }
+
+
+  
